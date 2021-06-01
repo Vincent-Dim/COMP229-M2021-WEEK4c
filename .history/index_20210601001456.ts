9f38c42467221853921c0ecacafa5e16ceb43383
Vincent Dim <vdim@my.centennialcollege.ca>
2021-06-01 00:44:10 -0400

    initial commit

---
+++ .history/index_20210601001456.ts
@@ -0,0 +1,24 @@
+import http from 'http';
+import path from 'path';
+import fs from 'fs';
+
+const hostname: string = '127.0.0.1';   //: type = value, don't have to do this
+const port: number = 3000;
+
+fs.readFile("index.html", (err,data) => 
+{
+  console.log(data);
+})
+
+const server: http.Server = http.createServer((req, res) =>  // user request and server response
+{
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain'); //sets up page header
+  res.end('Hello, World');          //server responds with hello world on web page
+});
+
+
+server.listen(port, hostname, () =>     //similar to add event lister on java script
+{
+  console.log(`Server running at http://${hostname}:${port}/`);
+}); 
---
+++ .history/index_20210601001828.ts
@@ -0,0 +1,29 @@
+import http from 'http';
+import path from 'path';
+import fs from 'fs';
+
+const hostname: string = '127.0.0.1';   //: type = value, don't have to do this
+const port: number = 3000;
+
+fs.readFile("index.html", (err,data) =>   //reading file index.html
+{
+  if(err)
+  {
+    console.log("Error"); //display error message if there is an error reading the file
+  }
+  
+  console.log(data);
+})
+
+const server: http.Server = http.createServer((req, res) =>  // user request and server response
+{
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain'); //sets up page header
+  res.end('Hello, World');          //server responds with hello world on web page
+});
+
+
+server.listen(port, hostname, () =>     //similar to add event lister on java script
+{
+  console.log(`Server running at http://${hostname}:${port}/`);
+}); 
