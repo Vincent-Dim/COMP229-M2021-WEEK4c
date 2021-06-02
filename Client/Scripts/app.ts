9be2f33b1c54d717d250dbacd6e0b6c2ca9ad083
Vincent Dim <vdim@my.centennialcollege.ca>
2021-06-01 20:51:57 -0400

    Added bootstrat and font-awesome support

---
+++ Client/Scripts/app.ts
@@ -0,0 +1,16 @@
+//IIFE -- immediately invoked function expression
+"use strict";
+
+(function(){
+
+    function Start():void
+    {
+        console.log("App Started");
+    }
+
+    window.addEventListener("load", Start);
+    
+
+ 
+
+})();
