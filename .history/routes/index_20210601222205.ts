8fff6b2cee8bc245d96da4fa1339100569917dc2
Vincent Dim <vdim@my.centennialcollege.ca>
2021-06-01 22:35:07 -0400

    Added simple NAV bar

--- .history/routes/index_20210601222205.ts
+++ .history/routes/index_20210601222205.ts
@@ -14,25 +14,21 @@ router.get('/home', function(req, res, next)  // so that entering http://localho
   res.render('index', { title: 'Home' });
 });
 
-/* GET about page. */
 router.get('/about', function(req, res, next) 
 {
   res.render('index', { title: 'About' });
 });
 
-/* GET projects page. */
 router.get('/projects', function(req, res, next) 
 {
   res.render('index', { title: 'Projects' });
 });
 
-/* GET services page. */
 router.get('/services', function(req, res, next) 
 {
   res.render('index', { title: 'Services' });
 });
 
-/* GET contact page. */
 router.get('/contact', function(req, res, next) 
 {
   res.render('index', { title: 'Contact' });
